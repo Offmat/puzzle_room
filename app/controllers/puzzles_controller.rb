d5a3c1409d4733936ce6c26695e50e97d1bc958c
Mateusz <lesniak.mat@gmail.com>
2018-01-20 15:02:55 +0100

    Create new puzzle in controller and views

--- app/controllers/puzzles_controller.rb
+++ app/controllers/puzzles_controller.rb
@@ -1,6 +1,27 @@
 class PuzzlesController < ApplicationController
+  def new
+    @puzzle = Puzzle.new
+    @producers = Company.all
+    @materials = Material.all
+    @inventors = Inventor.all
+  end
+
+  def create
+    @puzzle = Puzzle.new(puzzle_params)
+    if @puzzle.save
+      redirect_to @puzzle
+    else
+      render 'new'
+    end
+  end
 
   def index
     @puzzles = Puzzle.all
+  end
+
+  private
+
+  def puzzle_params
+    params.require(:puzzle).permit(:name, :description, :level, :company_id, :material_ids)
   end
 end
