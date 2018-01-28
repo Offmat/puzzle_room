29e869f085b8d22d2535f4e15b79190fc1b5f51a
Mateusz <lesniak.mat@gmail.com>
2018-01-28 16:31:07 +0100

    Load csv countries list

---
+++ lib/scripts/load_countries.rb
@@ -0,0 +1,8 @@
+require 'csv'
+require 'pry'
+
+file = File.expand_path("../csv_files/country_list.csv", __dir__)
+
+CSV.foreach(file, headers: true) do |row|
+  Country.new(name: row["name"]).save
+end
