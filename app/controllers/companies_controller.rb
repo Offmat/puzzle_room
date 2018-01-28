2af199bee3bbb41361b04d4392edfe4483c60d12
Mateusz <lesniak.mat@gmail.com>
2018-01-28 15:33:42 +0100

    Add companies features: show add delete

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,16 +1,32 @@
 class CompaniesController < ApplicationController
   before_action :find_company, only: [:show, :edit, :update, :destroy]
-  before_action :pre_form, only: [:new, :edit]
+  before_action :pre_form, only: [:new, :edit, :create]
 
   def index
     @model = Company.all
     render 'common/index'
   end
 
+  def show
+  end
+
   def new
+    @company = Company.new
   end
 
   def create
+    @company = Company.new(company_params)
+    if @company.save
+      redirect_to companies_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @company = Company.find(params[:id])
+    @company.destroy
+    redirect_to companies_path
   end
 
   private
