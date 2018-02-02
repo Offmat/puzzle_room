e829c5356ea1b4d26c27c86c0e7c19d0537300e3
Mateusz <lesniak.mat@gmail.com>
2018-02-02 20:57:26 +0100

    Set cemments user null and add Guest comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   validates :body, presence: true
 
-  belongs_to :user
+  belongs_to :user, optional: true
   belongs_to :commentable, polymorphic: true
 
   has_many :comments, as: :commentable
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180121175331) do
+ActiveRecord::Schema.define(version: 20180202195313) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 20180121175331) do
     t.text "body", null: false
     t.integer "commentable_id", null: false
     t.string "commentable_type", null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["commentable_type", "commentable_id"], name: "index_comments_on_commentable_type_and_commentable_id"
