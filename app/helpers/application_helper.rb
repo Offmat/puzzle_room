f17f25ecb0839bbfe0e007c525faa83f4f354183
Mateusz <lesniak.mat@gmail.com>
2018-02-03 20:03:56 +0100

    Add validation errors to company form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,8 @@ module ApplicationHelper
   def page_is_active(path)
     params[:controller] == path ? 'active' : ''
   end
+
+  def errors_list(object, field_name)
+    object.errors.messages[field_name]
+  end
 end
