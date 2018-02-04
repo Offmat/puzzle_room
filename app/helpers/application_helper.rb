1711817150af4e85edea0b788ed0e5841a1957f4
Mateusz <lesniak.mat@gmail.com>
2018-02-04 18:17:45 +0100

    Set view for editing user account

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,8 +2,4 @@ module ApplicationHelper
   def page_is_active(path)
     params[:controller] == path ? 'active' : ''
   end
-
-  def errors_list(object, field_name)
-    object.errors.messages[field_name]
-  end
 end
