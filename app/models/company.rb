1719c214c389027720cb14eeacc4603a6af89934
Mateusz <lesniak.mat@gmail.com>
2018-02-04 18:56:29 +0100

    Refract some views and add some links

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,7 @@
 class Company < ApplicationRecord
 validates :name, presence: true
+validates :country, presence: {message: "need to be chosen"}
 
-belongs_to :country
+belongs_to :country, optional: true
 has_many :puzzles
 end
--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -1,10 +1,11 @@
 class Puzzle < ApplicationRecord
   validates :name, presence: true, uniqueness: { case_sensitive: false }
   validates :level, presence: true, inclusion: { in: 1..6, message: "is not in range 1-6" }
+  validates :producer, presence: {message: "need to be chosen"}
   has_many :makes, dependent: :destroy
   has_many :materials, through: :makes
   belongs_to :inventor, optional: true
-  belongs_to :producer, foreign_key: 'company_id', class_name: 'Company'
+  belongs_to :producer, foreign_key: 'company_id', class_name: 'Company', optional:true
 
   has_many :possesions, dependent: :destroy
   has_many :owners, through: :possesions, source: :user
