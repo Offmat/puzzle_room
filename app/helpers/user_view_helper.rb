777aa9676757cef866c516e0c7ebf1a69cd56a5d
Mateusz <lesniak.mat@gmail.com>
2018-02-10 19:30:38 +0100

    Discard nil from most popular inventor and add some helpers

--- app/helpers/user_view_helper.rb
+++ app/helpers/user_view_helper.rb
@@ -1,4 +1,4 @@
-module UserHelper
+module UserViewHelper
   def most_popular_material(puzzles)
     all_materials = []
     puzzles.each do |puzzle|
@@ -11,3 +11,7 @@ module UserHelper
     end
   end
 end
+
+ def most_popular_inventor(puzzles)
+   puzzles.where.not(inventor: nil).group_by(&:inventor).max_by { |_k,v| v.length }.first&.full_name
+ end
